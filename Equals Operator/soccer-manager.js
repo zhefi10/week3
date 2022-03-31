0b7d2a9c1e6058547d219b5d4b60f0deea41ef9a
Erick Suárez <esuarez_97@hotmail.com>
2022-03-30 22:30:24 -0500

    Add files via upload
    Week 3 was interesting.

---
+++ Equals Operator/soccer-manager.js
@@ -0,0 +1,28 @@
+// Soccer manager activity
+//Create the function getPlayingStyle, return the team you think it has the best playing style.
+
+//Run the code in the console to find out what's the secondTeam final playing style.
+
+/////////////////////////
+//your code here
+function getPlayingStyle()
+{
+  const firstTeam = [3,3,1,3];
+  const secondTeam = firstTeam;
+
+  firstTeam[0]=4;
+  firstTeam[1]=1;
+  firstTeam[2]=4;
+  firstTeam[3]=1;
+
+  return secondTeam; 
+}
+
+/////////////////////////
+//Uncomment this line to see the results in the browser console
+console.log("results = " + getPlayingStyle())
+/////////////////////////
+//don't change the code under this line
+if (typeof module !== 'undefined') {
+  module.exports = getPlayingStyle;
+}
